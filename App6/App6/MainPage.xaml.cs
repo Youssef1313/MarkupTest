6a4bc8aa447b1ae33f7f70aff04232cd8e0500a7
Youssef Victor <youssefvictor00@gmail.com>
2023-03-16 20:06:06 +0200

    Add project files.

---
+++ App6/App6/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Markup;
+
+namespace App6
+{
+    [MarkupExtensionReturnType(ReturnType = typeof(UIElement))]
+    public class ChildElementExtension : MarkupExtension
+    {
+        public ChildElementExtension()
+        {
+        }
+
+        protected override object ProvideValue() => new TextBlock { Text = "asd" };
+    }
+
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            //var res = sp.Resources.Single();
+        }
+    }
+}
